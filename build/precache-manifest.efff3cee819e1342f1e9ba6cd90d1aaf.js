d39cc042c4238dbeeef505a0be07629ed56c9fd6
Sudhindra <sudheendrapai.b@gmail.com>
2021-01-12 01:24:26 +0530

    added frontend

---
+++ build/precache-manifest.efff3cee819e1342f1e9ba6cd90d1aaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fe669e7d24e14e661e1db444b91d0b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f868b9d9f48e38f849b0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a5dfc74143f1b4d1f142",
+    "url": "/static/js/2.c8fe3bc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c8fe3bc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f868b9d9f48e38f849b0",
+    "url": "/static/js/main.d9111056.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
